541c3eb4657c4cdb6f5e72b62c125bb95472a1d8
Arehone45 <arehonemulaudzi08@gmail.com>
2024-01-10 21:35:15 +0200

    added a resttemplate call to department-service from user

--- user-service/src/main/java/net/deptuserapp/userservice/service/serviceimpl/UserServiceImpl.java
+++ user-service/src/main/java/net/deptuserapp/userservice/service/serviceimpl/UserServiceImpl.java
@@ -1,18 +1,23 @@
 package net.deptuserapp.userservice.service.serviceimpl;
 
 import lombok.AllArgsConstructor;
+import net.deptuserapp.userservice.dto.DepartmentDto;
 import net.deptuserapp.userservice.dto.ResponseDto;
 import net.deptuserapp.userservice.dto.UserDto;
 import net.deptuserapp.userservice.entity.User;
 import net.deptuserapp.userservice.repository.UserRepository;
 import net.deptuserapp.userservice.service.UserService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 @AllArgsConstructor
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private RestTemplate restTemplate;
+
     @Override
     public User saveUser(User user) {
 
@@ -24,10 +29,16 @@ public class UserServiceImpl implements UserService {
 
         ResponseDto responseDto = new ResponseDto();
         User user = userRepository.findById(userId).get();
-
         UserDto userDto = mapToUser(user);
 
-        responseDto.setUserDto(userDto);
+        ResponseEntity<DepartmentDto> responseEntity = restTemplate
+                .getForEntity("http://localhost:8080/department-app/getDepartmentById/" + user.getDepartmentId(),
+                        DepartmentDto.class);
+
+        DepartmentDto departmentDto = responseEntity.getBody();
+
+        responseDto.setDepartment(departmentDto);
+        responseDto.setUser(userDto);
 
 
         return responseDto;
